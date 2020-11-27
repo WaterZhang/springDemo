7a17c9e2824e10e4599b28fcea375e2b720d0864
Water Zemiao Zhang <wzhang@expedia.com>
2020-11-27 14:28:17 +0800

    update

--- src/main/java/com/zhangzemiao/www/springdemo/web/SwaggerConfiguration.java
+++ src/main/java/com/zhangzemiao/www/springdemo/web/SwaggerConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.RequestParameterBuilder;
-import springfox.documentation.schema.ModelSpecification;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ParameterType;
